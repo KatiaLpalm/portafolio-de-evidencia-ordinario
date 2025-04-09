6d3fefa888d3096d5f653c4113cd91beeafe1355
KATIA ALEXANDRA PALMA LOPEZ <202260688@ucc.mx>
2025-04-09 09:00:12 -0600

    Add files via upload

---
+++ Archivo2.js
@@ -0,0 +1,7 @@
+//const saludame = require("./Archivo.js");
+//console.log (Saludame);
+
+const {sayHello, sayHelloWorld} = require('./Archivo.js');
+
+console.log (sayHello('Mau'));
+console.log(sayHelloWorld());
